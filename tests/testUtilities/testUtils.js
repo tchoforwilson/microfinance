67a05f99d184c61564a7d077d7861c2e56a36a9d
tchoforwilson <tchoforwilson@gmail.com>
2022-03-17 06:36:19 +0100

    Test utilities for integration tests

---
+++ tests/testUtilities/testUtils.js
@@ -0,0 +1,10 @@
+/**
+ * Generate a sign token
+ * @param {String} id -> Token payload
+ * @returns Object
+ */
+export const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
