f45faadbae38bd431b3b3fb31cab13f9156080f6
tchoforwilson <tchoforwilson@gmail.com>
2022-03-17 13:39:36 +0100

    UserController_Tests \n Test_CreateUser It Should return 400 for Invalid role \n Test_CreateUser It Should return 201 if the user was successfully created

--- tests/integrationTests/userTests/userController.test.js
+++ tests/integrationTests/userTests/userController.test.js
@@ -1,53 +1,124 @@
 "use strict";
 import request from "supertest";
-import bcrypt from "bcrypt";
 import * as RandomVal from "./../../testUtilities/GenRandomVal.js";
 import * as UnitTest from "./../../testUtilities/unit_testbases.js";
 import { signToken } from "./../../testUtilities/testUtils.js";
 import database from "./../../../config/database.js";
+import { before } from "lodash";
 
 const User = database.user;
 const Account = database.account;
+const Op = database.Sequelize.Op;
 
 describe("UserController_Tests", () => {
   const MAX = 32;
   // define variables globally needed
   let server;
   let adminUser = {};
-  let token;
+  let header;
   const createAdmin = async () => {
     const user = UnitTest.GenRandomValidUserWithPassword();
     user.password = "pass1234";
+    user.password_confirm = "pass1234";
     user.role = "manager";
-    user.passwordConfirm = "pass1234";
-    const adminUser = await User.create(user);
-    token = signToken(adminUser.user_id);
+    user.rights.push("createUser", "getUsers");
+    adminUser = await User.create(user);
+    const token = signToken(adminUser.user_id);
+    header = "Bearer " + token;
   };
   // 1. Call the server
-  beforeEach(async () => {
+  beforeAll(async () => {
     const mod = await import("../../../index");
     server = mod.default;
+    await User.sequelize.sync();
+    await Account.sequelize.sync();
     await createAdmin();
   });
 
-  afterEach(async () => {
+  afterAll(async () => {
     if (server) {
-      server.close();
-      await User.destroy({ where: {}, truncate: false });
-      await Account.destroy({ where: {}, truncate: false });
+      await server.close();
     }
   });
-  describe("/api/v1/users", () => {
-    it("Test_CreateUser It Should return 400 for Invalid role", () => {
+  afterEach(async () => {
+    await User.destroy({
+      where: { [Op.not]: [{ user_id: adminUser.user_id }] },
+      truncate: false,
+    });
+    await Account.destroy({
+      where: { [Op.not]: [{ user_id: adminUser.user_id }] },
+      truncate: false,
+    });
+  });
+  describe("POST /api/v1/users", () => {
+    it("Test_CreateUser It Should return 400 for Invalid role", async () => {
+      //await createAdmin();
       const user = UnitTest.GenRandomValidUser();
       user.role = "manager";
-      const header = "Bearer " + token;
-      const res = request(server)
+      const res = await request(server)
         .post("/api/v1/users/")
         .send(user)
-        .set("authorization", token);
+        .set("Authorization", header);
 
       expect(res.status).toBe(400);
     });
+    it("Test_CreateUser It Should return 201 if the user was successfully created", async () => {
+      //await createAdmin();
+      const user = UnitTest.GenRandomValidUser();
+      const res = await request(server)
+        .post("/api/v1/users/")
+        .send(user)
+        .set("Authorization", header);
+      const data = JSON.parse(res.text);
+      expect(res.status).toBe(201);
+      expect(data.status).toBe("success");
+      expect(data.data.data).not.toBe.null;
+      expect(data.data.data.account).not.toBe.null;
+      expect(data.data.data.account.balance).toBe(0);
+    });
+  });
+  describe("GET /api/v1/users", () => {
+    it("Test_GetAllUsers It should return 200 for documents found", async () => {
+      const res = await request(server)
+        .get("/api/v1/users/")
+        .set("Authorization", header);
+      const data = JSON.parse(res.text);
+      expect(res.status).toBe(200);
+      expect(data.status).toBe("success");
+      expect(data.data.docs.count).toBe(1);
+      // TODO: check that it is a n array
+      //expect(data.data.docs.rows).toBeArray();
+    });
   });
 });
+
+// describe("UserController_Tests", () => {
+//   beforeAll(() => {
+//     console.log("Initialize server");
+//   });
+//   afterAll(() => {
+//     console.log("Close server");
+//   });
+//   beforeEach(() => {
+//     console.log("Create database");
+//   });
+//   afterEach(() => {
+//     console.log("Close database");
+//   });
+//   describe("/api/v1/users 1", () => {
+//     it("1=> Test 1", () => {
+//       console.log("1=> Inner Test 1");
+//     });
+//     it("1=> Test 2", () => {
+//       console.log("1=> Inner Test 2");
+//     });
+//   });
+//   describe("/api/v1/users 2", () => {
+//     it("2=> Test 1", () => {
+//       console.log("2=> Inner Test 1");
+//     });
+//     it("2=> Test 2", () => {
+//       console.log("2=> Inner Test 2");
+//     });
+//   });
+// });
