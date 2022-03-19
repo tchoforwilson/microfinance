7db4c237a576284ef7f817776d14e43f129ed095
tchoforwilson <tchoforwilson@gmail.com>
2022-03-19 08:55:57 +0100

    Test_UpdateZone It return 404 for zone not found

--- tests/integrationTests/zoneTests/zoneController.test.js
+++ tests/integrationTests/zoneTests/zoneController.test.js
@@ -82,75 +82,87 @@ describe("ZoneController_Tests", () => {
       expect(returnZone.description).toBe(zone.description);
       expect(returnZone.user_id).toBe(zone.user_id);
     });
-    describe("GET /api/v1/zone", () => {
-      it("Test_GetAllZones It should return 404 for zones not found", async () => {
-        // 1. send request
-        const res = await request(server)
-          .get("/api/v1/zone")
-          .set("Authorization", header);
+  });
+  describe("GET /api/v1/zone", () => {
+    it("Test_GetAllZones It should return 404 for zones not found", async () => {
+      // 1. send request
+      const res = await request(server)
+        .get("/api/v1/zone")
+        .set("Authorization", header);
 
-        expect(res.status).toBe(404);
+      expect(res.status).toBe(404);
+    });
+    it("Test_GetAllZones It should return 200 if zones are found", async () => {
+      // 1. Generate random valid users
+      const genUsers = UnitTest.GenRandValidUsers(4);
+      // 2. Populate database with users
+      const users = await User.bulkCreate(genUsers);
+      // 3. Get users ids
+      let userIds = [];
+      users.forEach((el) => {
+        userIds.push(el.user_id);
       });
-      it("Test_GetAllZones It should return 200 if zones are found", async () => {
-        // 1. Generate random valid users
-        const genUsers = UnitTest.GenRandValidUsers(4);
-        // 2. Populate database with users
-        const users = await User.bulkCreate(genUsers);
-        // 3. Get users ids
-        let userIds = [];
-        users.forEach((el) => {
-          userIds.push(el.user_id);
-        });
-        // 4. Generate random valid zones
-        const genZones = UnitTest.GenRandomValidZones(MIN, userIds);
-        // 5. Populate database with zones
-        await Zone.bulkCreate(genZones);
+      // 4. Generate random valid zones
+      const genZones = UnitTest.GenRandomValidZones(MIN, userIds);
+      // 5. Populate database with zones
+      await Zone.bulkCreate(genZones);
 
-        // 6. send request
-        const res = await request(server)
-          .get("/api/v1/zone")
-          .set("Authorization", header);
+      // 6. send request
+      const res = await request(server)
+        .get("/api/v1/zone")
+        .set("Authorization", header);
 
-        // 7. Expect result
-        expect(res.status).toBe(200);
-        const data = JSON.parse(res.text);
-        const returnZones = data.data.docs;
-        expect(returnZones.count).toEqual(MIN);
-        returnZones.rows.forEach((zone) => {
-          Object.keys(zone).forEach((el) => {
-            expect(zone).toHaveProperty(el, zone[el]);
-          });
+      // 7. Expect result
+      expect(res.status).toBe(200);
+      const data = JSON.parse(res.text);
+      const returnZones = data.data.docs;
+      expect(returnZones.count).toEqual(MIN);
+      returnZones.rows.forEach((zone) => {
+        Object.keys(zone).forEach((el) => {
+          expect(zone).toHaveProperty(el, zone[el]);
         });
       });
     });
-    describe("GET /api/v1/zone/:id", () => {
-      it("Test_GetZone It should return 404 for zone not found", async () => {
-        // 1. Generate random valid id
-        const id = RandomVal.GenRandomInteger(MAX);
-        // 2. Send request
-        const res = await request(server)
-          .get(`/api/v1/zone/${id}`)
-          .set("Authorization", header);
-        // 3. Expect result
-        expect(res.status).toBe(404);
-      });
-      it("Test_GetZone It should return 200 for zone found", async () => {
-        // 1. Generate random zone
-        const genZone = UnitTest.GenRandomValidZone(adminUser.user_id);
-        // 2. Create zone in table
-        const zone = await Zone.create(genZone);
-        const zoneData = zone.dataValues;
-        // 2. Send request
-        const res = await request(server)
-          .get(`/api/v1/zone/${zone.zone_id}`)
-          .set("Authorization", header);
-        // 3. Expect result
-        expect(res.status).toBe(200);
-        const data = JSON.parse(res.text);
-        const returnZone = data.data.data;
-        expect(returnZone.zone_id).toEqual(zoneData.zone_id);
-        expect(returnZone.user_id).toEqual(zoneData.user_id);
-      });
+  });
+  describe("GET /api/v1/zone/:id", () => {
+    it("Test_GetZone It should return 404 for zone not found", async () => {
+      // 1. Generate random valid id
+      const id = RandomVal.GenRandomInteger(MAX);
+      // 2. Send request
+      const res = await request(server)
+        .get(`/api/v1/zone/${id}`)
+        .set("Authorization", header);
+      // 3. Expect result
+      expect(res.status).toBe(404);
+    });
+    it("Test_GetZone It should return 200 for zone found", async () => {
+      // 1. Generate random zone
+      const genZone = UnitTest.GenRandomValidZone(adminUser.user_id);
+      // 2. Create zone in table
+      const zone = await Zone.create(genZone);
+      const zoneData = zone.dataValues;
+      // 2. Send request
+      const res = await request(server)
+        .get(`/api/v1/zone/${zone.zone_id}`)
+        .set("Authorization", header);
+      // 3. Expect result
+      expect(res.status).toBe(200);
+      const data = JSON.parse(res.text);
+      const returnZone = data.data.data;
+      expect(returnZone.zone_id).toEqual(zoneData.zone_id);
+      expect(returnZone.user_id).toEqual(zoneData.user_id);
+    });
+  });
+  describe("PATCH /api/v1/zone/:id", () => {
+    it("Test_UpdateZone It return 404 for zone not found", async () => {
+      // 1. Generate random valid id
+      const id = RandomVal.GenRandomInteger(MAX);
+      // 2. Send request
+      const res = await request(server)
+        .patch(`/api/v1/zone/${id}`)
+        .set("Authorization", header);
+      // 3. Expect result
+      expect(res.status).toBe(404);
     });
   });
 });
