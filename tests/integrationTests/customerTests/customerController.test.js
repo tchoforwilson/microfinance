25b337293629622f33822b085beccdafcb904252
tchoforwilson <tchoforwilson@gmail.com>
2022-03-28 15:22:39 +0100

    Test_DeleteCustomer It should return 404 for customer not found

--- tests/integrationTests/customerTests/customerController.test.js
+++ tests/integrationTests/customerTests/customerController.test.js
@@ -155,4 +155,18 @@ describe("CustomerController_Tests", () => {
       expect(updateCustomer.contact).toEqual(returnedCustomer.contact);
     });
   });
+  describe("DELETE /api/v1/customers/:id", () => {
+    it("Test_DeleteCustomer It should return 404 for customer not found", async () => {
+      // 1. Generate random number as id,
+      const id = RandomVal.GenRandomInteger(MAX);
+
+      // 2. Send request
+      const res = await request(server)
+        .delete(`/api/v1/customers/${id}`)
+        .set("Authorization", header);
+
+      // 3. Expect results
+      expect(res.status).toBe(404);
+    });
+  });
 });
