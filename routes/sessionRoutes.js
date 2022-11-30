748cb5775e37850ce5997b7bbb60d5fc65f5388a
tchoforwilson <tchoforwilson@gmail.com>
2022-11-30 18:54:23 +0100

    Deleted

--- routes/sessionRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-import express from "express";
-import * as sessionController from "./../controllers/sessionController.js";
-const router = express.Router();
-
-router.post("/open", sessionController.openSession);
-router.patch("/close", sessionController.closeSession);
-
-export default router;
