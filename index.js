99e4d9ce6cf45391817d07ffa475b387c6664ee9
tchoforwilson <tchoforwilson@gmail.com>
2023-02-22 14:44:16 +0100

    file refactoring

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-"use strict";
-process.on("uncaughtException", (err) => {
-  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err);
   process.exit(1);
 });
 
-import app from "./app.js";
-import { connectDB } from "./config/connect.js";
+// eslint-disable-next-line import/first
+import app from './app.js';
+// eslint-disable-next-line import/first
+import { connectDB } from './config/connect.js';
 
 // connect to the database if we are in development or production
 const env = process.env.NODE_ENV;
 
-if (env === "production") {
-  await connectDB(env, {});
+if (env === 'production') {
+  connectDB(env, {});
 }
-if (env === "development") {
-  await connectDB(env, {});
+if (env === 'development') {
+  connectDB(env, {});
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}....`);
 });
 
-process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLER REJECTION! 💥 Shutting down...");
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLER REJECTION! 💥 Shutting down...');
   console.log(err);
   server.close(() => {
     process.exit(1);
