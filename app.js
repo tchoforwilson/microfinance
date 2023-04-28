e911a43cbc9e67dc052dbf40708108977791ee34
tchoforwilson <tchoforwilson@gmail.com>
2023-04-28 09:40:34 +0100

    refactored

--- app.js
+++ app.js
@@ -1,40 +1,38 @@
-"use strict";
 import path from 'path';
-import {fileURLToPath} from 'url';
-import cors from "cors";
-import express, { json, urlencoded } from "express";
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
-import sessionRouter from "./routes/sessionRoutes.js";
-import userRouter from "./routes/userRoutes.js";
-import zoneRouter from "./routes/zoneRoutes.js";
-import customerRouter from "./routes/customerRoutes.js";
-import accountRouter from "./routes/accountRoutes.js";
-import sourceRouter from "./routes/sourceRoutes.js";
-import transactionRouter from "./routes/transactionRoutes.js";
-import loanRouter from "./routes/loanRoutes.js";
-import AppError from "./utils/appError.js";
-import globalErrorHandler from "./controllers/errorController.js";
+import { fileURLToPath } from 'url';
+import cors from 'cors';
+import express, { json, urlencoded } from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+
+import userRouter from './routes/userRoutes.js';
+import zoneRouter from './routes/zoneRoutes.js';
+import customerRouter from './routes/customerRoutes.js';
+import accountRouter from './routes/accountRoutes.js';
+import transactionRouter from './routes/transactionRoutes.js';
+import AppError from './utils/appError.js';
+import globalErrorHandler from './controllers/errorController.js';
+
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
 // Serving static files
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 //app.use('/static',express.static(path.join(new URL('public', import.meta.url))));
-app.use('/public',express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Development logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 // Body parser, reading data from body into req.body
-app.use(json({ limit: "10kb" }));
-app.use(urlencoded({ extended: true, limit: "10kb" }));
+app.use(json({ limit: '10kb' }));
+app.use(urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: process.env.API_ENDPOINT,
 };
 
 app.use(cors(corsOptions));
@@ -46,16 +44,13 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
-app.use("/api/v1/session", sessionRouter);
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/zones", zoneRouter);
-app.use("/api/v1/customers", customerRouter);
-app.use("/api/v1/accounts", accountRouter);
-app.use("/api/v1/sources", sourceRouter);
-app.use("/api/v1/transactions", transactionRouter);
-app.use("/api/v1/loans", loanRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/zones', zoneRouter);
+app.use('/api/v1/customers', customerRouter);
+app.use('/api/v1/accounts', accountRouter);
+app.use('/api/v1/transactions', transactionRouter);
 // INVALID ROUTES
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
